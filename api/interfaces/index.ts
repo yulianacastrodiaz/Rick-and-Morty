8e23fbcb19a514ff1db3f6233bc109a9568e7b46
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-09-11 19:27:31 -0500

    Interfaces

--- api/interfaces/index.ts
+++ api/interfaces/index.ts
@@ -3,3 +3,42 @@ export interface error {
 	message: string;
 }
 
+export interface CharactersResponse {
+	info: Info;
+	results: Character[]
+}
+
+export interface Info{
+	count: number;
+	pages: number;
+	next: string | null;
+	prev: string | null;
+}
+
+export interface Character {
+	id: number;
+	name: string;
+	status: string;
+	species: string;
+	type: string;
+	gender: string;
+	origin: Location;
+	location: Location;
+	image: string;
+	episode: string[];
+	url: string;
+	created: string;
+}
+
+export interface Location{
+	name: string;
+	url: string;
+}
+
+export interface MappedCharacter{
+	id: number;
+	name: string;
+	gender: string;
+	species: string;
+	image: string;
+}
